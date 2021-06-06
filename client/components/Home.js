8e28bb9237e5406731c6b800f63af604eb94f004
Maria Fernanda <mfht95@gmail.com>
2021-06-06 16:39:26 -0500

    navbar set up

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,14 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react";
 
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
-
+export const Home = (props) => {
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <h3>Welcome</h3>
     </div>
-  )
-}
+  );
+};
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    username: state.auth.username
-  }
-}
-
-export default connect(mapState)(Home)
+export default Home;
--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,47 +1,60 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>FS-App-Template</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+const Navbar = () => (
+  <div className="sidenav">
+    <br></br>
+    <br></br>
+    <Link to="/">
+      <h1 id="navbar-maria-fernanda">☆ María Fernanda</h1>
+    </Link>
+    <h4>
+      software engineer, artist, <p></p> game designer, 3D animator
+    </h4>
+    <br></br>
+    <br></br>
+    <br></br>
+    <nav className="navbar-first-links">
+      <Link to="/" id="first-link">
+        Code
+      </Link>
+      <br></br>
+      <Link to="/" id="first-link">
+        Animation
+      </Link>
+      <br></br>
+      <Link to="/" id="first-link">
+        Games
+      </Link>
+      <br></br>
+      <Link to="/" id="first-link">
+        Misc
+      </Link>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Link to="/" id="first-link">
+        About Me
+      </Link>
+      <br></br>
+      <Link to="/" id="first-link">
+        Artst Statement
+      </Link>
+      <br></br>
+      <Link to="/" id="first-link">
+        Resumes
+      </Link>
+      <br></br>
+      <Link to="/" id="first-link">
+        Vimeo / itch.io
+      </Link>
+      <br></br>
+      <Link to="/" id="first-link">
+        Github / LinkedIn
+      </Link>
+      <br></br>
     </nav>
-    <hr />
   </div>
-)
+);
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.auth.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Navbar)
+export default Navbar;
